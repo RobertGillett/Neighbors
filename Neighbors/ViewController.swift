79ed38a59f38c476618be392f4dca4e4b6d69034
Robert Gillett <rob@cannactrl.com>
2020-02-05 09:44:15 -0800

    Initial Commit

---
+++ Neighbors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Neighbors
+//
+//  Created by Robert Gillett on 2/5/20.
+//  Copyright © 2020 Robert Gillett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NeighborsTests/NeighborsTests.swift
@@ -0,0 +1,34 @@
+//
+//  NeighborsTests.swift
+//  NeighborsTests
+//
+//  Created by Robert Gillett on 2/5/20.
+//  Copyright © 2020 Robert Gillett. All rights reserved.
+//
+
+import XCTest
+@testable import Neighbors
+
+class NeighborsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
